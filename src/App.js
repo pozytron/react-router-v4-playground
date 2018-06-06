fe2c8008d75c7695e2b39d2e38915a06c3e484b8
Michał Tymuła <michal.tymula@coderslab.pl>
2018-06-06 16:31:48 +0200

    changed declarations to classes

--- src/App.js
+++ src/App.js
@@ -1,70 +1,77 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const BasicExample = () => (
-  <Router>
-    <div>
+class BasicExample extends React.Component{
+  render(){
+    return <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  }
+}
+class Home extends React.Component {
+    render(){
+      return <div>
+                <h2>Homeish</h2>
+             </div>
+    }
+}
+class About extends React.Component {
+    render(){
+      return <div>
+                <h2>Something to thing about</h2>
+             </div>
+    }
+}
+class Topics extends React.Component {
+  render(){
+    return <div>
+      <h2>Topics</h2>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to={`${this.props.match.url}/rendering`}>Rendering with React</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to={`${this.props.match.url}/components`}>Components</Link>
         </li>
         <li>
-          <Link to="/topics">Topics</Link>
+          <Link to={`${this.props.match.url}/props-v-state`}>Props v. State</Link>
         </li>
       </ul>
 
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path={`${this.props.match.url}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={this.props.match.url}
+        render={() => <h3>Please select a topic.</h3>}
+      />
     </div>
-  </Router>
-);
+  }
+}
+class Topic extends React.Component {
+    render(){
+      return <div>
+                <h3>{this.props.match.params.topicId}</h3>
+             </div>
+    }
+}
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
 
 export default BasicExample;
