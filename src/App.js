6a660833b2625457c05de374f612e3de274c25b7
Michał Tymuła <michal.tymula@coderslab.pl>
2018-06-07 10:17:58 +0200

    comments and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+import {Route, Link} from "react-router-dom";
 
+// Main component
 class BasicExample extends React.Component{
   render(){
     return <Router>
@@ -18,14 +20,17 @@ class BasicExample extends React.Component{
         </ul>
 
         <hr />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+        <section>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </section>
       </div>
     </Router>
   }
 }
+
+// Simple regular boring components
 class Home extends React.Component {
     render(){
       return <div>
@@ -40,13 +45,15 @@ class About extends React.Component {
              </div>
     }
 }
+
+// Nested routes and dynamic rendering
 class Topics extends React.Component {
   render(){
     return <div>
       <h2>Topics</h2>
       <ul>
         <li>
-          <Link to={`${this.props.match.url}/rendering`}>Rendering with React</Link>
+          <Link to={`${this.props.match.url}/givenParam`}>Rendering with React</Link>
         </li>
         <li>
           <Link to={`${this.props.match.url}/components`}>Components</Link>
@@ -57,16 +64,16 @@ class Topics extends React.Component {
       </ul>
 
       <Route path={`${this.props.match.url}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={this.props.match.url}
-        render={() => <h3>Please select a topic.</h3>}
+      <Route exact path={this.props.match.url}
+        render={
+           () => <h3>Please select a topic.</h3>
+        }
       />
     </div>
   }
 }
 class Topic extends React.Component {
-    render(){
+    render(){ // show me what U got...
       return <div>
                 <h3>{this.props.match.params.topicId}</h3>
              </div>
